f5d193faaf036149876113a38198ae53127aa1ff
Im Kyeongbin <lkbzbcg15@gmail.com>
2023-05-01 17:08:51 +0900

    리드미 포함

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, g
-from extern_lib.AI_16_CP2.face_ds_project import FaceDSProject
+from .extern_lib.AI_16_CP2.face_ds_project import FaceDSProject
 
 app = Flask(__name__)
 face_ds_project = FaceDSProject()
@@ -16,5 +16,5 @@ def hello_world():
     return 'Hello World!'
 
 if __name__ == '__main__':
-    # app.run(host='0.0.0.0', debug=True)
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', debug=True)
+    # app.run(host='0.0.0.0')
---
+++ readme.md
@@ -0,0 +1,5 @@
+`flask run`
+는 바로 내려가고
+
+`python -m flask run`
+안 내려감
