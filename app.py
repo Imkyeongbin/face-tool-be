3edd51dc6ec6c652e651a074d89d13e1fac5b1eb
Im Kyeongbin <lkbzbcg15@gmail.com>
2023-05-02 15:13:27 +0900

    waitress로 서버 올림

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
 from flask import Flask, g, render_template
-from .extern_lib.AI_16_CP2.face_ds_project import FaceDSProject
-import tracemalloc
 
-tracemalloc.start()
+
+import os
+import sys
+
+# 스크립트 파일의 절대 경로를 구합니다.
+script_file_path = os.path.abspath(__file__)
+
+project_root = os.path.join(os.path.dirname(script_file_path), '..')
+sys.path.insert(0, project_root)
+
+from extern_lib.AI_16_CP2.face_ds_project import FaceDSProject
+# import tracemalloc
+
+# tracemalloc.start()
 
 app = Flask(__name__, template_folder='templates/dist', static_folder='templates/dist', static_url_path='')
 face_ds_project = FaceDSProject()
@@ -11,7 +22,7 @@ face_ds_project = FaceDSProject()
 def add_face_ds_project_to_g():
     g.face_ds_project = face_ds_project
 
-from .views import api
+from views import api
 app.register_blueprint(api.bp, url_prefix='/api')
 
 @app.route('/', defaults={'path': ''})
@@ -20,11 +31,6 @@ def catch_all(path):
     return render_template("index.html")
 
 if __name__ == '__main__':
-    try:
-        # app.run(host='0.0.0.0', debug=True)
-        app.run(host='0.0.0.0')
-    finally:
-        # 애플리케이션이 종료되는 시점에 메모리 할당 정보 출력
-        current, peak = tracemalloc.get_traced_memory()
-        print(f"Current memory usage: {current / 10**6}MB; Peak usage: {peak / 10**6}MB")
-        tracemalloc.stop()
+    # app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0')
+    
