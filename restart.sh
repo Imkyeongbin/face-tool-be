a285a29bce8b25c5fae514ba758fe50073a03a95
Lucas Im <fjeidjenk38@gmail.com>
2024-01-05 09:55:54 +0000

    crontab 사용할 때, 파이썬과 쉘스크립트 둘 다 실행 디렉토리에 영향을 받기 때문에, 작업 디렉토리를 `cd`로 변경해야 함.

--- restart.sh
+++ restart.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+### 실행될 스크립트 위치에 맞춰 변경할 것. ###
+SCRIPT_PATH=~/programming/face-tool-be
+
+
+# 스크립트를 실행할 위치로 변경
+cd $SCRIPT_PATH
+
+# Conda 환경을 초기화
+source ~/miniconda3/etc/profile.d/conda.sh
+
 # run.sh 프로세스 종료
 # 예를 들어, pkill, kill, systemctl stop 등의 명령어 사용
 pkill -f 'waitress-serve'
@@ -11,4 +21,4 @@ sleep 5
 conda activate ai16cp2
 
 # run.sh 스크립트 다시 시작
-./run.sh &
+bash ./run.sh & >> ~/logs/restart.log 2>&1
